8218042a906d24ee941254bfaa41f39e2929a76d
Emre Erkesikbaş <emre.erkesikbas@haratres.com>
2025-07-20 18:27:00 +0300

    feature/CMS-003 init docker and mysql database

---
+++ src/main/java/com/backend/shared/common/TenantContext.java
@@ -0,0 +1,21 @@
+package com.backend.shared.common;
+
+public class TenantContext {
+    private static final ThreadLocal<String> currentTenant = new ThreadLocal<>();
+    
+    public static void setCurrentTenant(String tenantId) {
+        currentTenant.set(tenantId);
+    }
+    
+    public static String getCurrentTenant() {
+        return currentTenant.get();
+    }
+    
+    public static void clear() {
+        currentTenant.remove();
+    }
+    
+    public static boolean hasTenant() {
+        return currentTenant.get() != null;
+    }
+}
