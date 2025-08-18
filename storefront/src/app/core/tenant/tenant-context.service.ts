d79c6c5ae6c46969e9c8d4b11c217ff754910a57
Emre Erkesikbaş <emre.erkesikbas@haratres.com>
2025-08-19 01:51:32 +0300

    feature/CMS-014 custom componentlerin geçtiği yerlerde düzenlemeler yapıldı

--- storefront/src/app/core/tenant/tenant-context.service.ts
+++ storefront/src/app/core/tenant/tenant-context.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
 import { Tenant } from 'app/modules/admin/custom/tenants/tenants.types';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class TenantContextService {
