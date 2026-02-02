8ccafc7a2d86026d2641be966a3dc301a3f92b68
Emre Erkesikbaş <emre.erkesikbas@haratres.com>
2026-02-03 02:34:48 +0300

    bugfix/CMS-165 dialog-modal componentlerinde bugfix yapıldı

--- storefront/src/app/shared/components/spa-dialog/spa-dialog-content/spa-dialog-content.component.ts
+++ storefront/src/app/shared/components/spa-dialog/spa-dialog-content/spa-dialog-content.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
 import { DialogContentType } from '../spa-dialog.types';
 
 @Component({
     selector: 'spa-dialog-content',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, MatDialogModule],
     templateUrl: './spa-dialog-content.component.html',
     styleUrls: ['./spa-dialog-content.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
